fc0ff254c623d90a8a5e4e24e3863fe06c93defd
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-02-27 23:00:21 +0900

    Initial Commit

---
+++ RandomQuoteAndImages/RandomQuoteAndImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomQuoteAndImagesApp.swift
+//  RandomQuoteAndImages
+//
+//  Created by joe on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct RandomQuoteAndImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
